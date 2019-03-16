c112fd61ff1ed6bf411b948481e3d1d69e0ebd7f
hubenchang0515 <hubenchang0515@outlook.com>
2019-03-16 11:52:20 +0800

    nothing

--- common/log.c
+++ common/log.c
@@ -1,3 +1,27 @@
+/****************************************************************************************
+MIT License
+
+Copyright (c) 2019 Plan C
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+****************************************************************************************/
+
 #include "log.h"
 #include <stdarg.h>
 #include <stdio.h>
@@ -15,14 +39,14 @@ int set_log_file(const char* fp)
 {
     if(log_file != NULL)
     {
-        log_printf(LOG_ERROR, "log file had been set before.");
+        log_printf(LOG_ERROR, "Log file had been set before.");
         return EXIT_FAILURE;
     }
 
     log_file = fopen(fp, "a+");
     if(log_file == NULL)
     {
-        log_printf(LOG_ERROR, "open log file failure : %s.", strerror(errno));
+        log_printf(LOG_ERROR, "Open log file failure : %s.", strerror(errno));
         return EXIT_FAILURE;
     }
 
@@ -37,7 +61,7 @@ int close_log_file(void)
     }
     else
     {
-        log_printf(LOG_ERROR, "close log file failure : %s.", strerror(errno));
+        log_printf(LOG_ERROR, "Close log file failure : %s.", strerror(errno));
         return EXIT_FAILURE;
     }
     
