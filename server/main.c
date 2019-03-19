e6a110bffa8907c2dcda3655e275a17f12026a05
hubenchang0515 <hubenchang0515@outlook.com>
2019-03-19 14:32:57 +0800

    rename config key

--- server/main.c
+++ server/main.c
@@ -70,38 +70,38 @@ int main(int argc, char* argv[])
     for(gsize i = 0; groups[i] != NULL; i++)
     {
         /* get ip:port to ip:port */
-        char* from_ip = config_get_value(config, groups[i], "from_ip");
-        int from_port = atoi(config_get_value(config, groups[i], "from_port"));
+        char* true_client_ip = config_get_value(config, groups[i], "true_client_ip");
+        int true_client_port = atoi(config_get_value(config, groups[i], "true_client_port"));
 
-        char* to_ip = config_get_value(config, groups[i], "to_ip");
-        int to_port = atoi(config_get_value(config, groups[i], "to_port"));
+        char* proxy_client_ip = config_get_value(config, groups[i], "proxy_client_ip");
+        int proxy_client_port = atoi(config_get_value(config, groups[i], "proxy_client_port"));
 
         log_printf(LOG_INFO, "Config Proxy (%s) %s:%d ----- %s:%d", 
-                    groups[i], from_ip, from_port, to_ip, to_port);
+                    groups[i], true_client_ip, true_client_port, proxy_client_ip, proxy_client_port);
 
         /* bind port to listen true client */
         int uv_err = 0;
         uv_tcp_t true_client;
         uv_tcp_init(loop, &true_client);
         struct sockaddr_in addr;
-        uv_ip4_addr(from_ip, from_port, &addr);
+        uv_ip4_addr(true_client_ip, true_client_port, &addr);
         uv_err = uv_tcp_bind(&true_client, (const struct sockaddr *)&addr, 0);
         if(uv_err < 0)
         {
             log_printf(LOG_ERROR, "Bind %s:%d error : %s.",
-                        from_ip, from_port, uv_strerror(uv_err));
+                        true_client_ip, true_client_port, uv_strerror(uv_err));
             continue;
         }
 
         /* bind port to listen proxy client */
         uv_tcp_t proxy_client;
         uv_tcp_init(loop, &proxy_client);
-        uv_ip4_addr(to_ip, to_port, &addr);
+        uv_ip4_addr(proxy_client_ip, proxy_client_port, &addr);
         uv_err = uv_tcp_bind(&proxy_client, (const struct sockaddr *)&addr, 0);
         if(uv_err < 0)
         {
             log_printf(LOG_ERROR, "Bind %s:%d error : %s.",
-                        to_ip, to_port, uv_strerror(uv_err));
+                        proxy_client_ip, proxy_client_port, uv_strerror(uv_err));
             continue;
         }
         
@@ -110,14 +110,14 @@ int main(int argc, char* argv[])
         if(uv_err < 0)
         {
             log_printf(LOG_ERROR, "Listen %s:%d error : %s.",
-                        from_ip, from_port, uv_strerror(uv_err));
+                        true_client_ip, true_client_port, uv_strerror(uv_err));
         }
 
         uv_err = uv_listen((uv_stream_t*)&proxy_client, 100, proxy_client_has_connection);
         if(uv_err < 0)
         {
             log_printf(LOG_ERROR, "Listen %s:%d error : %s.",
-                        to_ip, to_port, uv_strerror(uv_err));
+                        proxy_client_ip, proxy_client_port, uv_strerror(uv_err));
         }
 
         /* bind user data */
